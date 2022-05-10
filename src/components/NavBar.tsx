d99e4b71c7bf956f927c13d0985028951479cdbb
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-10 18:18:02 +0100

    Nav Bar with links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconsBar } from "./Icons";
 
 export const NavBar = () => {
   const links = {
     home: {
-      href: "/",
+      href: "/home",
       text: "Home",
     },
     about: {
-      href: "/",
+      href: "/about",
       text: "About",
     },
     languages: {
@@ -16,17 +16,17 @@ export const NavBar = () => {
       text: "Skills",
     },
     projects: {
-      href: "/",
+      href: "/projects",
       text: "Projects",
     },
   };
   return (
-    <div className="w-[250px] h-full bg-zinc-900">
+    <div className="w-[250px] h-full bg-zinc-900 flex flex-col">
       <div className="pt-8 mx-6">
         <p className="text-xl font-bold tracking-wide">tom dev</p>
         <p className="text-sm">Aveiro, PT</p>
       </div>
-      <div>
+      <div className="flex flex-col">
         <div className="mx-6 mt-8">
           {Object.entries(links).map(([key, value]) => {
             console.log(key, value);

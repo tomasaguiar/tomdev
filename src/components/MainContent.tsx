9d160d172ffdbeefba8b2b63afd316d650ef4b04
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-11 10:31:44 +0100

    Margin fixed for mobile

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,5 +2,5 @@ interface Props {
   children: any;
 }
 export const MainContent: React.FC<Props> = ({ children }) => {
-  return <div className="pt-8 mx-6">{children}</div>;
+  return <div className="w-full mx-4 mt-8 md:mx-6">{children}</div>;
 };

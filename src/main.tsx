38da99b1ea8e7cd578307cec9c564a3003f4dd1f
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-11 10:32:04 +0100

    React DOM Routes added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import App from "./pages/App";
 import "./global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Home from "./pages/home";
+import Skills from "./pages/Skills";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );

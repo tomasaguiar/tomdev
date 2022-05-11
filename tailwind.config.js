222e37e432bfc8b07f25264586619c2b74750e60
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-11 10:30:03 +0100

    New Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        letterings: {
+          100: "#878787",
+          200: "#b3b3b3",
+          300: "#d9d9d9",
+        },
+        ComponentsBackground: {
+          100: "#1f1f1f",
+          200: "#2f2f2f",
+        },
+      },
+    },
   },
   plugins: [],
 };

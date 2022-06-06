0ac440ecf412a5bb0cf7a4e497ea1ed6a8bfb34f
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-06 12:51:02 +0100

    Dropdown fix

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -18,21 +18,21 @@ export const Dropdown: React.FC<DropdownProps> = (props) => {
   return (
     <div className="flex justify-center">
       <Menu as="div" className="relative flex w-full text-left">
-        <Menu.Button as="div" className="flex-1 outline-none">
+        <Menu.Button as="div" className="rounded-md outline-none ">
           <AiOutlineMenu className="text-xl cursor-pointer hover:bg-ComponentsBackground-200" />
         </Menu.Button>
 
         <Menu.Items
-          as="a"
+          as="div"
           className={clsx(
-            "relative z-50 w-full mt-48 focus:outline-none rounded-md"
+            "relative z-50 w-full mt-48 focus:outline-none rounded"
           )}
         >
           {props.items.map((item) => (
             <Menu.Item
               as="a"
               href={item.href}
-              className="block w-20 p-2 rounded-md first-line:h-10 bg-ComponentsBackground-100 hover:bg-ComponentsBackground-200"
+              className="block p-2 w-28 first-line:h-10 bg-ComponentsBackground-100 hover:bg-ComponentsBackground-200"
             >
               <div className="flex flex-row items-center">
                 <div className="mr-2">{item.icon}</div>

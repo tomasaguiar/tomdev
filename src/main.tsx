d24880a661709cd582c70112caabd6fa4e75461e
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-07 00:43:29 +0100

    Route bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import "./global.css";
 
-import Home from "./pages/Home";
+// import Home from "./pages/home";
 import Skills from "./pages/Skills";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -17,7 +17,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/home" element={<Home />} />
+      {/* <Route path="/home" element={<Home />} /> */}
       <Route path="/skills" element={<Skills />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/about" element={<About />} />

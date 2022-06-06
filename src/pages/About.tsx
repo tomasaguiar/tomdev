984a3316ed4fd226f186540dbf0fe56c6c1f0dc4
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-07 00:41:06 +0100

    About Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,10 +8,20 @@ export default function About() {
       <div className="flex flex-col h-screen md:flex-row">
         <NavBar active="home" />
         <MainContent>
-          <div className="mt-4">
+          <div className="mx-2 md:mr-[0px] mr-10">
             <h1 className="text-2xl font-bold">Who am I?</h1>
             <div className="">
-              <p className="text-letterings-300">Basic description about me.</p>
+              <p className="text-letterings-300">
+                A technology enthusiast, software developer. With 3 years of
+                experience, I have worked with industry leaders and developed
+                solutions to improve people's daily lives. Passionate about
+                creating software and observe its effect on improving something
+                or someone in the world.
+              </p>
+              <p className="mt-4 text-letterings-300">
+                To discover my background and projects, visit my GitHub page
+                where do I introduce some of my software.
+              </p>
             </div>
           </div>
         </MainContent>

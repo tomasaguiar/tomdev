aaed54b209589fbb6e29c3636cfe8d239c62f7a1
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-07 00:41:16 +0100

    Dropdown Tweaks WIP

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -17,26 +17,26 @@ export interface DropdownProps {
 export const Dropdown: React.FC<DropdownProps> = (props) => {
   return (
     <div className="flex justify-center">
-      <Menu as="div" className="relative flex w-full text-left">
+      <Menu as="div" className="relative flex w-full text-left rounded">
         <Menu.Button as="div" className="rounded-md outline-none ">
-          <AiOutlineMenu className="text-xl cursor-pointer hover:bg-ComponentsBackground-200" />
+          <AiOutlineMenu className="text-xl cursor-pointer hover:bg-[#303544]" />
         </Menu.Button>
 
         <Menu.Items
           as="div"
           className={clsx(
-            "relative z-50 w-full mt-48 focus:outline-none rounded"
+            "relative z-50 w-full mt-48 focus:outline-none rounded shadow-lg ring-1 ring-black ring-opacity-5"
           )}
         >
           {props.items.map((item) => (
             <Menu.Item
               as="a"
               href={item.href}
-              className="block p-2 w-28 first-line:h-10 bg-ComponentsBackground-100 hover:bg-ComponentsBackground-200"
+              className="block p-2 w-44 first-line:h-10 bg-[#20222D] hover:bg-[#303544]"
             >
               <div className="flex flex-row items-center">
-                <div className="mr-2">{item.icon}</div>
-                <div>{item.name}</div>
+                <div className="mx-2 my-[2px]">{item.icon}</div>
+                <div className="text-sm">{item.name}</div>
               </div>
             </Menu.Item>
           ))}

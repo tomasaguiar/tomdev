95d6f7d8dda7d75bf9697910796ee86c13067ef9
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-07 00:40:46 +0100

    Projects Page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,17 +1,24 @@
 import { Bubbles } from "../components/Bubbles";
 import { MainContent } from "../components/MainContent";
 import { NavBar } from "../components/NavBar";
+import { ProjectComponent } from "../components/Projects";
 
 export default function Projects() {
   return (
-    <div className="h-screen bg-black">
-      <div className="flex flex-col h-screen md:flex-row">
+    <div className="h-full bg-black md:h-screen">
+      <div className="flex flex-col h-full md:h-screen md:flex-row">
         <NavBar active="home" />
         <MainContent>
-          <div className="mt-4">
+          <div className="mx-2 md:mr-[0px] mr-10">
             <h1 className="text-2xl font-bold">What i've done?</h1>
             <div className="">
-              <p className="text-letterings-300">This is my projects</p>
+              <p className="text-letterings-300">
+                This was my best projects, check my github to check a more few
+                of them.
+              </p>
+            </div>
+            <div className="mt-4 md:flex md:flex-row">
+              <ProjectComponent />
             </div>
           </div>
         </MainContent>
